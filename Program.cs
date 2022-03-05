4e81fce125df4bbcc1e6bb515a93ce169a5baecf
luarkim <parkleeh31@gmail.com>
2022-03-04 22:21:45 -0300

    tarefa realizada

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Mongágua");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Santos");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Peruibe");
+Console.WriteLine("Sao Vicente");
